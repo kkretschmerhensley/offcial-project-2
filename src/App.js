8c0cd89b3fad1e705476d568cd917d3a83632e7d
kkretschmerhensley <k.kretschmerhensley@gmail.com>
2021-02-15 13:03:03 -0600

    Added new styles to the DOM

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import './App.css';
 import {Addition} from './Addition.js';
 
 const App = () => {
+  const styles = {
+    container: {
+      padding: 20,
+      backgroundColor: "black",
+      color: "white"
+    }
+  };
+
   return (
-    <>
+    <div style={styles.container}>
       <Addition numberOne={12} numberTwo={4}/>
       <Addition numberOne={-14} numberTwo={25}/>
       <Addition numberOne={2.27} numberTwo={13.543}/>
-    </>
+    </div>
   );
 }
 
